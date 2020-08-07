0d8332434407e8025819833ff694dd3fa7044042
박준수 <pksjmh5295@gmail.com>
2020-08-07 12:30:40 +0900

    React Basic

--- movie_app_2020/src/App.js
+++ movie_app_2020/src/App.js
@@ -1,24 +1,23 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Potato from "./Potato";
+
+function Beef() {
+  return <h1>I love steak❤</h1>;
+}
+
+function Food({ fav }) {
+  // << props.fav와 같음
+  return <h1>I love {fav}❤</h1>;
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Hello World!!!!!!</h1>
+      {/* <Potato /> */}
+      {/* <Beef /> */}
+      <Food fav="kimchi" something={true} array={["hello", 1, 2, 3, 4]} />
+      <Food fav="ramen" />
     </div>
   );
 }
