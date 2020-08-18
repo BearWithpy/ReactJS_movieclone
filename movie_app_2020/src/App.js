f7a8ea378cba63e29c967733b079d0c5365f1f91
박준수 <pksjmh5295@gmail.com>
2020-08-18 09:09:25 +0900

    map recap

--- movie_app_2020/src/App.js
+++ movie_app_2020/src/App.js
@@ -5,38 +5,53 @@ function Beef() {
   return <h1>I love steak❤</h1>;
 }
 
-function Food({ name }) {
-  // << props.fav와 같음
-  return <h1>I love {name}❤</h1>;
+function Food({ name, picture }) {
+  // << props.name와 같음
+  return (
+    <div>
+      <h1>I love {name}❤</h1>
+      <img src={picture} alt={name}></img>{" "}
+    </div>
+  );
 }
 
 const foodIlike = [
   {
+    id: 1,
     name: "Kimchi",
     image:
       "http://aeriskitchen.com/wp-content/uploads/2008/09/kimchi_bokkeumbap_02-.jpg",
   },
   {
+    id: 2,
     name: "Samgyeopsal",
     image:
       "https://3.bp.blogspot.com/-hKwIBxIVcQw/WfsewX3fhJI/AAAAAAAAALk/yHxnxFXcfx4ZKSfHS_RQNKjw3bAC03AnACLcBGAs/s400/DSC07624.jpg",
   },
   {
+    id: 3,
     name: "Bibimbap",
     image:
       "http://cdn-image.myrecipes.com/sites/default/files/styles/4_3_horizontal_-_1200x900/public/image/recipes/ck/12/03/bibimbop-ck-x.jpg?itok=RoXlp6Xb",
   },
   {
+    id: 4,
     name: "Doncasu",
     image:
       "https://s3-media3.fl.yelpcdn.com/bphoto/7F9eTTQ_yxaWIRytAu5feA/ls.jpg",
   },
   {
+    id: 5,
     name: "Kimbap",
     image:
       "http://cdn2.koreanbapsang.com/wp-content/uploads/2012/05/DSC_1238r-e1454170512295.jpg",
   },
 ];
+
+function renderFood(dish) {
+  // console.log(dish);
+  return <Food name={dish.name} picture={dish.image} />;
+}
 
 function App() {
   return (
@@ -44,10 +59,11 @@ function App() {
       {/* <h1>Hello World!!!!!!</h1> */}
       {/* <Potato /> */}
       {/* <Beef /> */}
-      <Food name="kimchi" something={true} array={["hello", 1, 2, 3, 4]} />d
-      {foodIlike.map((dish) => (
-        <Food name={dish.name} />
-      ))}
+      {/* <Food name="kimchi" something={true} array={["hello", 1, 2, 3, 4]} />d */}
+      {/* {foodIlike.map((dish) => (
+        <Food name={dish.name} picture={dish.image} />
+      ))} */}
+      {foodIlike.map(renderFood)}
     </div>
   );
 }
