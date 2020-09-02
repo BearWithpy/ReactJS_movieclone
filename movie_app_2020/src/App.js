46a0acdbb1ba62c366ddf6ce75e735a8eb1c1691
박준수 <pksjmh5295@gmail.com>
2020-09-02 09:24:35 +0900

    Edit App.js file ... Movie Component

--- movie_app_2020/src/App.js
+++ movie_app_2020/src/App.js
@@ -1,42 +1,20 @@
 import React from "react";
-// import PropTypes from "prop-types";
 
 class App extends React.Component {
-  constructor(props) {
-    super(props);
-    console.log("hello");
-  }
   state = {
-    count: 0,
-  };
-
-  add = () => {
-    this.setState((current) => ({ count: current.count++ }));
-  };
-  sub = () => {
-    this.setState((current) => ({ count: current.count-- }));
-    // Using a "current" method is way better!
+    isLoading: true,
+    movies: [],
   };
 
   componentDidMount() {
-    console.log("Component rendered");
-  }
-  componentDidUpdate() {
-    console.log("I'm updated");
-  }
-  componentWillUnmount() {
-    console.log("Goodbye, cruel world");
+    setTimeout(() => {
+      this.setState({ isLoading: false });
+    }, 6000);
   }
 
   render() {
-    console.log("I'm rendering");
-    return (
-      <div>
-        <h1>The Number is {this.state.count}</h1>
-        <button onClick={this.add}>Plus</button>
-        <button onClick={this.sub}>Minus</button>
-      </div>
-    );
+    const { isLoading } = this.state;
+    return <div>{isLoading ? "Loading..." : "We are ready"}</div>;
   }
 }
 
--- movie_app_2020/src/index.js
+++ movie_app_2020/src/index.js
@@ -4,6 +4,7 @@ import App from "./App";
 
 ReactDOM.render(
   <React.StrictMode>
+    {/* StrictMode - when rendering it goes twice  */}
     <App />
   </React.StrictMode>,
   document.getElementById("potato")
