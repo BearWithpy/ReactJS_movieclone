b443ad9e04008a874b871394188a25863614d589
박준수 <pksjmh5295@gmail.com>
2020-09-03 13:40:36 +0900

    Edit Movie.js & App.js files

--- movie_app_2020/src/App.js
+++ movie_app_2020/src/App.js
@@ -1,12 +1,12 @@
 import React from "react";
 import axios from "axios";
+import Movie from "./Movie";
 
 class App extends React.Component {
   state = {
     isLoading: true,
     movies: [],
   };
-
   getMovies = async () => {
     const {
       data: {
@@ -17,14 +17,27 @@ class App extends React.Component {
     );
     this.setState({ movies, isLoading: false });
   };
-
   componentDidMount() {
     this.getMovies();
   }
-
   render() {
-    const { isLoading } = this.state;
-    return <div>{isLoading ? "Loading..." : "We are ready"}</div>;
+    const { isLoading, movies } = this.state;
+    return (
+      <div>
+        {isLoading
+          ? "Loading..."
+          : movies.map((movie) => (
+              <Movie
+                key={movie.id}
+                id={movie.id}
+                year={movie.year}
+                title={movie.title}
+                summary={movie.summary}
+                poster={movie.medium_cover_image}
+              />
+            ))}
+      </div>
+    );
   }
 }
 
